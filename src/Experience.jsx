4804645de36bf8b4e67c18c227da5b15b4feeade
Austin Papritz <austin@papritz.dev>
2024-05-19 11:01:47 -0700

    added menu, fix class to clasName, styling for menus

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -27,18 +27,18 @@ export default function Experience()
         <button className='btn prev' name="arrow-back-outline" onClick={handlePreviousView}>PREV</button>
         <button className='btn next' name="arrow-forward-outline" onClick={handleNextView}>NEXT</button>
       </nav>
-      <div class="menu-wrapper">
-        <button id="backBtn" class="btn" type="submit">BACK</button>
-        <div class="menu-content">
-          <form class="menu-form">
-            <label for="size" class="size-container">Size
-              <input class="size-input" name="size" type="number" min="5" max="16"/>
+      <div className="menu-wrapper">
+        <button id="backBtn" classNameName="btn" type="submit">BACK</button>
+        <div className="menu-content">
+          <form className="menu-form">
+            <label for="size" className="size-container">Size
+              <input className="size-input" name="size" type="number" min="5" max="16"/>
             </label>
-            <label for="color" class="color-container">Color
-              <div class="color-btn-wrapper">
-                <button class="blue-white color-btn"></button>
-                <button class="gold-white color-btn"></button>
-                <button class="red-white color-btn"></button>
+            <label for="color" className="color-container">Color
+              <div className="color-btn-wrapper">
+                <button className="blue-white color-btn"></button>
+                <button className="gold-white color-btn"></button>
+                <button className="red-white color-btn"></button>
               </div>
             <button type="submit" id="bundleBtn">Bundle to Save</button>
             </label>
